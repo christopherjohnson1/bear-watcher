9e02364fc58baa2078fb17777da78b1fcb6d68f4
Chris Johnson <christopherjohnson1@gmail.com>
2020-03-09 12:33:40 -0500

    bears added to river

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,15 @@
 import form from '../javascripts/components/bearForm.js'
+import river from './components/river.js'
+import bearData from './helpers/data/bearData.js'
+
+const addBearEvent = (e) => {
+    e.preventDefault();
+};
 
 const init = () => {
     form.printBearForm();
+    river.buildBears();
+    
 };
 
 init();
